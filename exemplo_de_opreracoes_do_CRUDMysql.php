57715e28572ca8715d47dec88c68c5c19fd2607c
RomuloSoaresBezerra <bromulosoares@gmail.com>
2017-09-01 23:10:46 -0300

    Arquivo de exemplo de Operacoes do CRUDMySql

---
+++ exemplo_de_opreracoes_do_CRUDMysql.php
@@ -0,0 +1,15 @@
+<?php
+	
+	include("conexaoMySql.php");
+	
+	//--- Inserindo dados
+	//Criando objeto com dados para inserção
+	$usuario = array( 
+		'nome' => 'Rômulo Soares Bezerra',  
+		'sexo' => 'Masculino', 
+		'profissao' => 'Estudante'
+	);
+	
+	
+	
+?>
