d0844fe749398d0bd74be0595397b29183732b71
RomuloSoaresBezerra <bromulosoares@gmail.com>
2017-09-10 18:54:36 -0300

    Cadastro do Bebê funcionando.

--- HiBaby/cadastroBebe.php
+++ HiBaby/cadastroBebe.php
@@ -13,6 +13,7 @@
 		//Inclui os arquivos
 		include("uploadFile.php");
 		include("crudMySql.php");
+		include("funcaoCadastraBebe.php");
 		
 		//Recebe parâmetros da requisicao
 		$nomeBebe = $_POST['nomeBebe'];
@@ -44,47 +45,5 @@
 					sweetAlert('Upload de foto mal sucedido', 'Tente novamente', 'error');
 					setTimeout(function() { location.href='principal.php' }, 3000);
   				 </script>";
-		
-		
-		//Função: cadastra o bebê
-		//Recebe: 
-		//  1- nome do bebê
-		//  2- nickname do bebê
-		//  3- data nascimento do bebê
-		//  4- sexo do bebê
-		//  5- caminho da foto do bebê
-		//  6- usuario responsavel pelo bebê
-		//Retorna: três diferentes resultados:
-		//  r1: se o cadastro for bem sucedido
-		//  r2: se o cadastro for mal sucedido
-		//  r3: se existir falha no upload da imagem
-		function cadastraBebe($nomeBebe, $nicknameBebe, $nascimentoBebe,
-			$sexoBebe, $fotoBebe, $usuarioResponsavel){
-			
-			define("retorno1", "CADASTRO_BEM_SUCEDIDO");
-			define("retorno2", "CADASTRO_MAL_SUCEDIDO");
-			define("retorno3", "UPLOAD_FOTO_MAL_SUCEDIDO");
-			
-			if($fotoBebe != NULL){
-		
-				$bebe = array( 
-					'nome' => $nomeBebe,  
-					'nickname' => $nicknameBebe, 
-					'nascimento' => $nascimentoBebe,
-					'sexo' => $sexoBebe,
-					'foto' => $fotoBebe
-				);
-			
-				$usuario_Bebe = array( 
-					'nicknameBebe' => $nicknameBebe, 
-					'email' => $usuarioResponsavel
-				);
-			
-				if(insert_database('Bebe', $bebe) && insert_database('Usuario_Bebe', $usuario_Bebe))
-					return retorno1;
-				return retorno2;
-				 
-			}else return retorno3;
-		}
 	
 	?>
---
+++ HiBaby/funcaoCadastraBebe.php
@@ -0,0 +1,43 @@
+<?php
+		//Função: cadastra o bebê
+		//Recebe: 
+		//  1- nome do bebê
+		//  2- nickname do bebê
+		//  3- data nascimento do bebê
+		//  4- sexo do bebê
+		//  5- caminho da foto do bebê
+		//  6- usuario responsavel pelo bebê
+		//Retorna: três diferentes resultados:
+		//  r1: se o cadastro for bem sucedido
+		//  r2: se o cadastro for mal sucedido
+		//  r3: se existir falha no upload da imagem
+		function cadastraBebe($nomeBebe, $nicknameBebe, $nascimentoBebe,
+			$sexoBebe, $fotoBebe, $usuarioResponsavel){
+			
+			define("retorno1", "CADASTRO_BEM_SUCEDIDO");
+			define("retorno2", "CADASTRO_MAL_SUCEDIDO");
+			define("retorno3", "UPLOAD_FOTO_MAL_SUCEDIDO");
+			
+			if($fotoBebe != NULL){
+		
+				$bebe = array( 
+					'nome' => $nomeBebe,  
+					'nickname' => $nicknameBebe, 
+					'nascimento' => $nascimentoBebe,
+					'sexo' => $sexoBebe,
+					'foto' => $fotoBebe
+				);
+			
+				$usuario_Bebe = array( 
+					'nicknameBebe' => $nicknameBebe, 
+					'email' => $usuarioResponsavel
+				);
+			
+				if(insert_database('Bebe', $bebe) && insert_database('Usuario_Bebe', $usuario_Bebe))
+					return retorno1;
+				return retorno2;
+				 
+			}else return retorno3;
+		}
+			
+?>
