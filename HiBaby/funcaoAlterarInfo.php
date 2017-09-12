337bb10791126744a1671b5abb67da488850e1b5
Thiago Yure Vieira de Almeida <thigoyure@gmail.com>
2017-09-11 22:24:05 -0300

    Refatorando função de alterar dados do usuário.

--- HiBaby/funcaoAlterarInfo.php
+++ HiBaby/funcaoAlterarInfo.php
@@ -3,14 +3,25 @@ function alterarDados($nome,$email,$foto,$cor,$senha,$emailAntigo){
   include("conexao.php");
   $conexao = open_database();
   if($conexao != null){
-    $sql = "UPDATE usuario SET email='$email',nome='$nome',foto='$foto',senha='$senha',cor='$cor' WHERE email='$emailAntigo'";
+    $sql1 = "SELECT * FROM usuario WHERE email='$email'"
+    if (mysqli_query($conexao,$sql1)) {
+      $result = mysqli_query($conexao,$sql1);
+      $numRows = mysqli_num_rows($result);
+      if ($numRows>=1) {
+        return false;
+      }else{
+        $sql = "UPDATE usuario SET email='$email',nome='$nome',foto='$foto',senha='$senha',cor='$cor' WHERE email='$emailAntigo'";
 
-    if(mysqli_query($conexao, $sql)){
-      return true;
+        if(mysqli_query($conexao, $sql)){
+          return true;
+        }else{
+          return false;
+        }
+        mysqli_close($conexao);
+      };
     }else{
       return false;
-    }
-    mysqli_close($conexao);
+    };
   }else{
     return false;
   }
