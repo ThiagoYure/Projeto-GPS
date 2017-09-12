6133373fd0acb6923fba245cd9799191bcd047e8
Thiago Yure Vieira de Almeida <thigoyure@gmail.com>
2017-09-11 22:49:52 -0300

    Refatorando funções de alteração de dados do bebe e do usuário.

--- HiBaby/funcaoAlterInfoBebe.php
+++ HiBaby/funcaoAlterInfoBebe.php
@@ -1,16 +1,38 @@
 <?php
-function alterarDadosBebe($nome,$nickname,$foto,$sexo,$nascimento,$nicknameBebe){
+function alterarDadosBebe($nome,$nickname,$foto,$sexo,$nascimento,$nicknameBebe,$emailUser){
   include("conexao.php");
   $conexao = open_database();
   if($conexao != null){
-    $sql = "UPDATE bebe SET nome='$nome',nickname='$nickname',foto='$foto',sexo='$sexo',nascimento='$nascimento' WHERE nickname='$nicknameBebe'";
+    if ($nickname==$nicknameBebe) {
+      $sql1 = "UPDATE bebe SET nome='$nome',nickname='$nickname',foto='$foto',sexo='$sexo',nascimento='$nascimento' WHERE nickname='$nicknameBebe'";
 
-    if(mysqli_query($conexao, $sql)){
-      return true;
+      if(mysqli_query($conexao, $sql1)){
+        return true;
+      }else{
+        return false;
+      }
+      mysqli_close($conexao);
     }else{
-      return false;
-    }
-    mysqli_close($conexao);
+      $sql2 = "SELECT * FROM usuario_bebe WHERE email='$emailUser' AND nicknameBebe='$nickname'"
+      if (mysqli_query($conexao,$sql2)) {
+        $result = mysqli_query($conexao,$sql2);
+        $numRows = mysqli_num_rows($result);
+        if ($numRows>=1) {
+          return false;
+        }else{
+          $sql3 = "UPDATE bebe SET nome='$nome',nickname='$nickname',foto='$foto',sexo='$sexo',nascimento='$nascimento' WHERE nickname='$nicknameBebe'";
+
+          if(mysqli_query($conexao, $sql3)){
+            return true;
+          }else{
+            return false;
+          }
+          mysqli_close($conexao);
+        };
+      }else{
+        return false;
+      };
+    };   
   }else{
     return false;
   }
--- HiBaby/funcaoAlterarInfo.php
+++ HiBaby/funcaoAlterarInfo.php
@@ -3,25 +3,36 @@ function alterarDados($nome,$email,$foto,$cor,$senha,$emailAntigo){
   include("conexao.php");
   $conexao = open_database();
   if($conexao != null){
-    $sql1 = "SELECT * FROM usuario WHERE email='$email'"
-    if (mysqli_query($conexao,$sql1)) {
-      $result = mysqli_query($conexao,$sql1);
-      $numRows = mysqli_num_rows($result);
-      if ($numRows>=1) {
-        return false;
-      }else{
-        $sql = "UPDATE usuario SET email='$email',nome='$nome',foto='$foto',senha='$senha',cor='$cor' WHERE email='$emailAntigo'";
+    if ($email==$emailAntigo) {
+      $sql1 = "UPDATE usuario SET email='$email',nome='$nome',foto='$foto',senha='$senha',cor='$cor' WHERE email='$emailAntigo'";
 
-        if(mysqli_query($conexao, $sql)){
-          return true;
-        }else{
-          return false;
-        }
-        mysqli_close($conexao);
-      };
+      if(mysqli_query($conexao, $sql1)){
+        return true;
+      }else{
+        return false;
+      }
+      mysqli_close($conexao);
     }else{
-      return false;
-    };
+      $sql2 = "SELECT * FROM usuario WHERE email='$email'"
+      if (mysqli_query($conexao,$sql2)) {
+        $result = mysqli_query($conexao,$sql2);
+        $numRows = mysqli_num_rows($result);
+        if ($numRows>=1) {
+          return false;
+        }else{
+          $sql3 = "UPDATE usuario SET email='$email',nome='$nome',foto='$foto',senha='$senha',cor='$cor' WHERE email='$emailAntigo'";
+
+          if(mysqli_query($conexao, $sql3)){
+            return true;
+          }else{
+            return false;
+          }
+          mysqli_close($conexao);
+        };
+      }else{
+        return false;
+      };
+    }
   }else{
     return false;
   }
